5474d4533939d7f60e869db11662bda815c7f21e
birdybro <aberusugi@gmail.com>
2022-08-01 22:59:20 -0600

    Cleanup in preparation to make things simpler

--- Nand2Tetris.sv
+++ Nand2Tetris.sv
@@ -202,33 +202,14 @@ wire [1:0] ar = status[122:121];
 assign VIDEO_ARX = (!ar) ? 12'd4 : (ar - 1'd1);
 assign VIDEO_ARY = (!ar) ? 12'd3 : 12'd0;
 
-`include "build_id.v" 
+`include "build_id.v"
 localparam CONF_STR = {
 	"Nand2Tetris;;",
 	"-;",
 	"O[122:121],Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
-	"O[2],TV Mode,NTSC,PAL;",
-	"O[4:3],Noise,White,Red,Green,Blue;",
 	"-;",
-	"P1,Test Page 1;",
-	"P1-;",
-	"P1-, -= Options in page 1 =-;",
-	"P1-;",
-	"P1O[5],Option 1-1,Off,On;",
-	"d0P1F1,BIN;",
-	"H0P1O[10],Option 1-2,Off,On;",
-	"-;",
-	"P2,Test Page 2;",
-	"P2-;",
-	"P2-, -= Options in page 2 =-;",
-	"P2-;",
-	"P2S0,DSK;",
-	"P2O[7:6],Option 2,1,2,3,4;",
-	"-;",
-	"-;",
-	"T[0],Reset;",
 	"R[0],Reset and close OSD;",
-	"V,v",`BUILD_DATE 
+	"V,v",`BUILD_DATE
 };
 
 wire forced_scandoubler;
@@ -248,7 +229,7 @@ hps_io #(.CONF_STR(CONF_STR)) hps_io
 	.buttons(buttons),
 	.status(status),
 	.status_menumask({status[5]}),
-	
+
 	.ps2_key(ps2_key)
 );
 
@@ -266,8 +247,6 @@ wire reset = RESET | status[0] | buttons[1];
 
 //////////////////////////////////////////////////////////////////
 
-wire [1:0] col = status[4:3];
-
 wire HBlank;
 wire HSync;
 wire VBlank;
@@ -279,8 +258,8 @@ Nand2Tetris Nand2Tetris
 (
 	.clk(clk_sys),
 	.reset(reset),
-	
-	.pal(status[2]),
+
+	// .pal(status[2]),
 	.scandouble(forced_scandoubler),
 
 	.ce_pix(ce_pix),
@@ -299,12 +278,12 @@ assign CE_PIXEL = ce_pix;
 assign VGA_DE = ~(HBlank | VBlank);
 assign VGA_HS = HSync;
 assign VGA_VS = VSync;
-assign VGA_G  = (!col || col == 2) ? video : 8'd0;
-assign VGA_R  = (!col || col == 1) ? video : 8'd0;
-assign VGA_B  = (!col || col == 3) ? video : 8'd0;
+// assign VGA_G  = (!col || col == 2) ? video : 8'd0;
+// assign VGA_R  = (!col || col == 1) ? video : 8'd0;
+// assign VGA_B  = (!col || col == 3) ? video : 8'd0;
 
-reg  [26:0] act_cnt;
-always @(posedge clk_sys) act_cnt <= act_cnt + 1'd1; 
-assign LED_USER    = act_cnt[26]  ? act_cnt[25:18]  > act_cnt[7:0]  : act_cnt[25:18]  <= act_cnt[7:0];
+// reg  [26:0] act_cnt;
+// always @(posedge clk_sys) act_cnt <= act_cnt + 1'd1; 
+// assign LED_USER    = act_cnt[26]  ? act_cnt[25:18]  > act_cnt[7:0]  : act_cnt[25:18]  <= act_cnt[7:0];
 
 endmodule
---
+++ rtl/Nand2Tetris_top.sv
@@ -0,0 +1,22 @@
+
+module Nand2Tetris
+(
+	input         clk,
+	input         reset,
+
+	// input         pal,
+	input         scandouble,
+
+	output reg    ce_pix,
+
+	output reg    HBlank,
+	output reg    HSync,
+	output reg    VBlank,
+	output reg    VSync,
+
+	output  [7:0] video
+);
+
+
+
+endmodule
