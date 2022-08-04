03b3f234effdbbab663f369315804693eaed7542
birdybro <aberusugi@gmail.com>
2022-08-03 22:44:59 -0600

    convert AND gate to using pure NAND

--- rtl/logic_gates/elementary/AND.v
+++ rtl/logic_gates/elementary/AND.v
@@ -12,6 +12,39 @@ module AND
 // | 1 | 0 | 0   |
 // | 1 | 1 | 1   | (a AND b)
 
-assign out = a & b;
+// (a AND b)
+// assign out = a & b;
+
+// NOT(a NAND b)
+// ((a NAND b) NAND (a NAND b))
+
+wire nand_1_out;
+
+NAND NAND_1
+(
+    .a(a),
+    .b(b),
+    .out(nand_1_out)
+);
+
+wire nand_2_out;
+
+NAND NAND_2
+(
+    .a(a),
+    .b(b),
+    .out(nand_2_out)
+);
+
+wire nand_3_out;
+
+NAND NAND_3
+(
+    .a(nand_1_out),
+    .b(nand_2_out),
+    .out(nand_3_out)
+);
+
+assign out = nand_3_out;
 
 endmodule
