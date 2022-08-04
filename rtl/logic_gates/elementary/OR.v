d0b98a82e40850b197c5dc4e2d54d8d9b6b6b466
birdybro <aberusugi@gmail.com>
2022-08-03 23:08:21 -0600

    convert OR gate to using pure NAND

--- rtl/logic_gates/elementary/OR.v
+++ rtl/logic_gates/elementary/OR.v
@@ -15,7 +15,38 @@ module OR
 // (NOT(a) AND b) OR (a AND NOT(b)) OR (a AND b)
 // NOT(NOT(a) and NOT(b))
 // (a OR b)
+// assign out = a | b;
 
-assign out = a | b;
+// NOT(NOT(a) AND NOT(b))
+// ((a NAND a) NAND (b NAND b))
+
+wire nand_1_out;
+
+NAND NAND_1
+(
+    .a(a),
+    .b(a),
+    .out(nand_1_out)
+);
+
+wire nand_2_out;
+
+NAND NAND_2
+(
+    .a(b),
+    .b(b),
+    .out(nand_2_out)
+);
+
+wire nand_3_out;
+
+NAND NAND_3
+(
+    .a(nand_1_out),
+    .b(nand_2_out),
+    .out(nand_3_out)
+);
+
+assign out = nand_3_out;
 
 endmodule
